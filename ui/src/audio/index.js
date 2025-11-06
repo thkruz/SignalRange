a62922d936986c9f0368711efe3cd4ca75bb4a49
Theodore Kruczek <theodore.kruczek@gmail.com>
2025-11-06 06:11:23 -0500

    refactor: :coffin: remove original ui

--- ui/src/audio/index.js
+++
@@ -1,8 +0,0 @@
-import switchSound from './switch.flac';
-import breakerSound from './breaker.wav';
-import errorSound from './error.flac';
-import raytubeOnSound from './raytubeOn.wav';
-import selectSound from './select.wav';
-import staticSound from './static.wav';
-
-export { switchSound, breakerSound, errorSound, raytubeOnSound, selectSound, staticSound };
--- ui/src/crud/index.js
+++
@@ -1,1 +0,0 @@
-export { CRUDdataTable } from './crud';
